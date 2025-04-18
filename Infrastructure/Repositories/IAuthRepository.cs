380c2914df4380fe602ecb57f8b0bc8cdeb1cdbf
MISBAH-UL-HAQ <misbah.26-it-2017@mietjammu.in>
2025-04-18 14:48:15 +0530

    jwt2

--- Infrastructure/Repositories/IAuthRepository.cs
+++ Infrastructure/Repositories/IAuthRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Repositories
 {
-    internal class IAuthRepository
+    public interface IAuthRepository
     {
+
+        Task<AuthResponseDto> RegisterAsync(RegisterUserDto dto);
+        Task<AuthResponseDto> LoginAsync(UserLoginDto dto);
+        Task<AuthResponseDto> LogOutAsync();
+
     }
 }
